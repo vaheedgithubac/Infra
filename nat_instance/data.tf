759b8f80153e4c2bbda9d5ce1e411ba223456b1a
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-10 02:03:17 +0530

    Create data.tf

---
+++ nat_instance/data.tf
@@ -0,0 +1,15 @@
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
+data "aws_ami" "amzn_linux_2023_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
