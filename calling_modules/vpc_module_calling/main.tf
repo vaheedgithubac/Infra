a2c9b2763d3c903c3fc0c71a4f6d33dde36db3c9
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-10 16:23:09 +0530

    Create main.tf

---
+++ calling_modules/vpc_module_calling/main.tf
@@ -0,0 +1,22 @@
+
+module "vpc" {
+  source =  # "./modules/vpc"  Give the path to VPC MODULE accordingly
+
+  #region      = var.region
+
+  project_name = var.project_name
+  env          = var.env
+  vpc_cidr     = var.vpc_cidr
+  common_tags  = var.common_tags
+
+  # All the counts should be same 
+  azs_count             = 2
+  public_subnet_count   = 2
+  private_subnet_count  = 2
+  database_subnet_count = 2
+
+  public_subnet_cidr   = var.public_subnet_cidr
+  private_subnet_cidr  = var.private_subnet_cidr
+  database_subnet_cidr = var.database_subnet_cidr
+
+}
