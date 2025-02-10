3eaf4e4762e9c8876f5eacc9f6c61dcb4dffa5f8
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-10 16:52:19 +0530

    Update main.tf

--- calling_modules/security_group_calling/main.tf
+++ calling_modules/security_group_calling/main.tf
@@ -1,15 +1,4 @@
 module "mysql_sg" {
-    # source = "git::https://github.com/daws-81s/terraform-aws-vpc.git?ref=main"
-    source = #"../terraform-aws-security-group"   Give the path accordingly
-    project_name = var.project_name
-    environment = var.environment
-    sg_name = "mysql"
-    vpc_id = module.vpc.vpc_id
-    common_tags = var.common_tags
-    sg_tags = var.mysql_sg_tags
-}
-
-module "mysql_sg" {
     source = "../terraform-aws-securitygroup"
     project_name = var.project_name
     environment = var.environment
