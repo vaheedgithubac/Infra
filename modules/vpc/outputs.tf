89b9ddd961f00882fefcad8430603ad4ce38c9e9
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-10 16:16:32 +0530

    Create outputs.tf

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,22 @@
+
+output "project_name" {
+  value = var.project_name
+}
+
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id  
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private.*.id
+}
+
+# This return entire internet_gateway object, if you want only id then use "aws_internet_gateway.internet_gateway.id"
+output "internet_gateway" {
+  value = aws_internet_gateway.internet_gateway
+}
+
