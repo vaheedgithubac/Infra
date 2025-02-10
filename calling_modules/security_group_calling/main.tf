2acc3363a160fa3643d3b7603336f433cf7c6090
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-10 16:49:44 +0530

    Update main.tf

--- calling_modules/security_group_calling/main.tf
+++ calling_modules/security_group_calling/main.tf
@@ -8,3 +8,13 @@ module "mysql_sg" {
     common_tags = var.common_tags
     sg_tags = var.mysql_sg_tags
 }
+
+module "mysql_sg" {
+    source = "../terraform-aws-securitygroup"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "mysql"
+    sg_description = "Created for MySQL instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
