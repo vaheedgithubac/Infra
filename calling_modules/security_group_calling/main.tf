7ef9e8a6e54fdc6ee3bc7adbde72c0a157caa736
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-10 19:27:26 +0530

    Update main.tf

--- calling_modules/security_group_calling/main.tf
+++ calling_modules/security_group_calling/main.tf
@@ -7,3 +7,12 @@ module "mysql_sg" {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = var.common_tags
 }
+
+resource "aws_security_group_rule" "allow_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]         #[var.remoteip]
+  security_group_id = module.mysql_sg.sg_id   #aws_security_group.sg_nat_instance.id
+}
