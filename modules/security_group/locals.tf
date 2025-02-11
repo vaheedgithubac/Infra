ecdfafe8bbae2ff39d8eed816892b16baabab7fe
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-11 13:06:24 +0530

    Update locals.tf

--- modules/security_group/locals.tf
+++ modules/security_group/locals.tf
@@ -1,3 +1,3 @@
 locals {
-    resource_final_name = "${var.project_name}-${var.environment}-${var.sg_name}"            # expense-dev-<sg_name>
+    resource_final_name = "${var.project_name}-${var.env}-${var.sg_name}"            # expense-dev-<sg_name>
 }
