792ea8b43af28a1b29b8894138991d279d70922d
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-11 12:48:30 +0530

    Update locals.tf

--- modules/ec2/locals.tf
+++ modules/ec2/locals.tf
@@ -1,4 +1,4 @@
 locals {
-    ec2_final_name = "${var.project_name}-${var.env}-ec2"
-    root_volume_final_name = "${var.project_name}-${var.env}-root-volume"
+    resorce_final_name = "${var.project_name}-${var.env}-ec2"                                   # expense-dev-ec2
+    root_volume_final_name = "${var.project_name}-${var.env}-root-volume"                       # expense-dev-rrot-volume
 }
