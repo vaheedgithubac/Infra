d5652cb56e7c622df82bdf6ce6cd0d4ff301636b
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-11 16:44:43 +0530

    Update main.tf

--- calling_modules/security_group_calling/main.tf
+++ calling_modules/security_group_calling/main.tf
@@ -1,11 +1,11 @@
-module "mysql_sg" {
-    source = "../terraform-aws-securitygroup"
-    project_name = var.project_name
-    environment = var.environment
-    sg_name = "mysql"
-    sg_description = "Created for MySQL instances in expense dev"
-    vpc_id = data.aws_ssm_parameter.vpc_id.value
-    common_tags = var.common_tags
+module "sg" {
+  source = "git::https://github.com/vaheedgithubac/Infra.git//modules/security_group"
+  project_name   = var.project_name
+  env            = var.env
+  sg_name        = var.sg_name
+  sg_description = var.sg_description
+  vpc_id         = var.vpc_id
+  common_tags    = var.common_tags
 }
 
 resource "aws_security_group_rule" "allow_http" {
@@ -13,6 +13,16 @@ resource "aws_security_group_rule" "allow_http" {
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]         #[var.remoteip]
-  security_group_id = module.mysql_sg.sg_id   #aws_security_group.sg_nat_instance.id
+  cidr_blocks       = ["0.0.0.0/0"]   #[var.remoteip]
+  security_group_id = module.sg.sg_id #aws_security_group.sg_nat_instance.id
 }
+
+# APP ALB accepting traffic from bastion
+# resource "aws_security_group_rule" "app_alb_bastion" {
+#  type              = "ingress"
+#  from_port         = 80
+#  to_port           = 80
+#  protocol          = "tcp"
+#  source_security_group_id       = module.bastion_sg.sg_id
+#  security_group_id = module.app_alb_sg.sg_id
+# }
