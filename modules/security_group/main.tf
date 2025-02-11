11fcf85894cf3fbaf8cc754f4ec5eb387e1faaa9
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-11 13:03:56 +0530

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "main" {
-  name        = local.sg_final_name
+  name        = local.resource_final_name
   description = var.sg_description
   vpc_id      = var.vpc_id
 
