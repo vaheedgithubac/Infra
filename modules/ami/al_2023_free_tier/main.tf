a57cdb1175d71946600b3790bb2879aae3545633
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-11 13:15:31 +0530

    Update main.tf

--- modules/ami/al_2023_free_tier/main.tf
+++ modules/ami/al_2023_free_tier/main.tf
@@ -1,13 +1,3 @@
-data "aws_ami" "amzn_linux_2023_ami" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023.*-x86_64"]
-  }
-}
-
 data "aws_ami" "base_ami" {
   most_recent      = true
   owners           = ["amazon"]
@@ -28,3 +18,16 @@ data "aws_ami" "base_ami" {
   }
 
 }
+
+######################################################################
+# data "aws_ami" "amzn_linux_2023_ami" {
+# most_recent = true
+#  owners      = ["amazon"]
+
+#  filter {
+#    name   = "name"
+#   values = ["al2023-ami-2023.*-x86_64"]
+#  }
+# }
+
+
