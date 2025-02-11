12feb83da74b6873b8ce2efe09b4041bf91a75d8
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-11 16:52:23 +0530

    Update data.tf

--- calling_modules/security_group_calling/data.tf
+++ calling_modules/security_group_calling/data.tf
@@ -1,3 +1,3 @@
-data "aws_ssm_parameter" "vpc_id" {
-  name = "/${var.project_name}/${var.environment}/vpc_id"
-}
+# data "aws_ssm_parameter" "vpc_id" {
+#  name = "/${var.project_name}/${var.environment}/vpc_id"
+# }
