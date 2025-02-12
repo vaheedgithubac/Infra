acec7f08c765a9f87c5618c2a2ecd8bd2f38cd39
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-12 20:01:55 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -109,7 +109,10 @@ resource "aws_route_table_association" "public" {
   
   subnet_id      = aws_subnet.public[count.index].id                      
   route_table_id = aws_route_table.public.id
-
+  
+   lifecycle {
+    create_before_destroy = true  # Ensures new association is created before destroying the old one
+  }
 }
 
 
