01ce450f4e1fc928c5043395399672927a512b45
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-16 12:29:38 +0530

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -10,3 +10,7 @@ output "instance_id" {
 output "primary_network_interface_id" {
   value = aws_instance.ec2_instance.primary_network_interface_id
 }
+
+output "is_nat_instance" {
+  value = var.is_nat_instance
+}
