cf1548501e8cabaf8f17b1776903b70bd4973562
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-16 13:14:44 +0530

    Update sg_for_nat_and_private_instances.tf

--- modules/nat_instance/sg_for_nat_and_private_instances.tf
+++ modules/nat_instance/sg_for_nat_and_private_instances.tf
@@ -1,5 +1,5 @@
 # Security group for NAT Instance
-module "sg" {
+module "nat_sg" {
     source = "git::https://github.com/vaheedgithubac/Infra.git//modules/security_group"
     project_name = var.project_name
     env = var.env
@@ -10,7 +10,7 @@ module "sg" {
 }
 
 # Security group for Private Subnet Instances
-module "sg" {
+module "private_sg" {
     source = "git::https://github.com/vaheedgithubac/Infra.git//modules/security_group"
     project_name = var.project_name
     env = var.env
@@ -27,7 +27,7 @@ resource "aws_security_group_rule" "remote_admin" {
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = [var.remote_ip_to_connect_nat_instance]   #[var.remoteip]
-  security_group_id = module.sg.sg_id     # aws_security_group.sg_nat_instance.id
+  security_group_id = module.nat_sg.sg_id     # aws_security_group.sg_nat_instance.id
 }
 
 # NAT Instance security group rule to allow all traffic from within the VPC
@@ -37,18 +37,18 @@ resource "aws_security_group_rule" "vpc-inbound" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = [var.vpc_cidr]
-  security_group_id = module.sg.sg_id    # aws_security_group.sg_nat_instance.id
+  security_group_id = module.nat_sg.sg_id    # aws_security_group.sg_nat_instance.id
 }
 
 # NAT Instance security group rule to allow outbound traffic
-resource "aws_security_group_rule" "outbound-nat-instance" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = module.sg.sg_id    # aws_security_group.sg_nat_instance.id
-}
+# resource "aws_security_group_rule" "outbound-nat-instance" {
+#  type              = "egress"
+#  from_port         = 0
+#  to_port           = 0
+#  protocol          = "-1"
+#  cidr_blocks       = ["0.0.0.0/0"]
+#  security_group_id = module.sg.sg_id    # aws_security_group.sg_nat_instance.id
+#}
 
 # Private instance security group rule to allow all traffic from public subnets 
 resource "aws_security_group_rule" "private-subnet1-inbound" {
@@ -56,16 +56,16 @@ resource "aws_security_group_rule" "private-subnet1-inbound" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks       = [var.public_subnets]   # [var.cidr_blocks[3]]
-  security_group_id = module.sg.sg_id    # aws_security_group.sg_test_instance.id
+  cidr_blocks       = var.public_subnet_cidr   # [var.cidr_blocks[3]]
+  security_group_id = module.private_sg.sg_id    # aws_security_group.sg_test_instance.id
 }
 
 # Private instance security group rule to allow outbound access
-resource "aws_security_group_rule" "outbound-test-instance" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = module.sg.sg_id   # aws_security_group.sg_test_instance.id
-}
+# resource "aws_security_group_rule" "outbound-test-instance" {
+#   type              = "egress"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = module.private_sg.sg_id   # aws_security_group.sg_test_instance.id
+# }
