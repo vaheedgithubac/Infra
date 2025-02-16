f6d9db2a410e91648a0ca7c2ff9511ff531c44ad
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-16 13:37:45 +0530

    Create outputs.tf

---
+++ calling_modules/nat_instance_calling/outputs.tf
@@ -0,0 +1,12 @@
+output "is_nat_instance" {
+  value = module.nat_instance.is_nat_instance
+}
+
+output "nat_instance_id" {
+  value = module.nat_instance.instance_id
+}
+
+output "primary_network_interface_id" {
+  value = module.nat_instance.primary_network_interface_id
+}
+
