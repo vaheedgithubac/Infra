55b4bb307775c52fba57a6b425ace6e4cdf60e18
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-16 11:35:25 +0530

    Delete modules/ec2/user_data.sh

--- modules/ec2/user_data.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-sudo yum install iptables-services -y
-sudo systemctl enable iptables
-sudo systemctl start iptables
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo /sbin/iptables -t nat -A POSTROUTING -o enX0 -j MASQUERADE
-# sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo /sbin/iptables -F FORWARD
