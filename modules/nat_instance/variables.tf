1ef18ce9020717227d42592dc431f5a5db3de9bc
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-16 13:15:52 +0530

    Update variables.tf

--- modules/nat_instance/variables.tf
+++ modules/nat_instance/variables.tf
@@ -1,27 +1,31 @@
-variable "resource_name" {}
+################  NAT Instance variables  ####################################
 variable "vpc_id" {}
-variable "private_rout" {}
-variable "private_subnet_cidr" {}
-
-variable "common_tags" {
-    type = map
-}
+variable "public_subnet_cidr" { type = list(any) }
+variable "private_subnet_ids" { type = list }
+  
 ############### Common variables for all Modules ############################
 variable "project_name" {}
 variable "env" {}
+variable "common_tags" {
+    type = map
+}
 ############### SG-MODULE related variables   ################################
 # variable "project_name" {}
 # variable "env" {}
 # variable "common_tags" { type = map }
-variable "vpc_id" {}
+# variable "vpc_id" {}  already declared above for other purpose
 variable "remote_ip_to_connect_nat_instance" {}
 variable "vpc_cidr" {}
-variable "public_subnets" { type = list }
+
 
 #############  EC2-MODULE related variables   #################################
 # variable "project_name" {}
 # variable "env" {}
+# variable "common_tags" { type = map }
 variable "ami_id" {}
 variable "public_key_name" {}
 variable "public_subnet_ID_to_launch_nat_instance" {}
 
+variable "is_nat_instance" { default = false }
+
+  
