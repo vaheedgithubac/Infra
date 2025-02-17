ac21dd25469d15bf9c810f7266cb324702556327
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-17 11:56:21 +0530

    Update main.tf

--- modules/aws_key_pair/main.tf
+++ modules/aws_key_pair/main.tf
@@ -10,7 +10,7 @@ resource "aws_key_pair" "generated" {
   provisioner "local-exec" {
     command = <<-EOF
       echo "Current working Directory......{path.module}"
-      echo "${tls_private_key.tf_generated_private_key.private_key_pem}" > ${path.module}\tf_aws_key_pair.pem
+      echo "${tls_private_key.tf_generated_private_key.private_key_pem}" > ${path.module}/tf_aws_key_pair.pem
       chmod 400 tf_aws_key_pair.pem
       echo "Private key created at ${path.module}/tf_aws_key_pair.pem"
     EOF
