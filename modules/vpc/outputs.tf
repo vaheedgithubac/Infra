dbfecf4a77eb51d94336f2e06e15874cb705e7d8
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-17 13:33:11 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -14,7 +14,19 @@ output "private_subnet_ids" {
   value = aws_subnet.private.*.id
 }
 
-# This return entire internet_gateway object, if you want only id then use "aws_internet_gateway.internet_gateway.id"
+# This returns entire internet_gateway object, if you want only id then use "aws_internet_gateway.internet_gateway.id"
 output "internet_gateway" {
   value = aws_internet_gateway.internet_gateway
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public.id  
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private.id  
+}
+
+output "database_route_table_id" {
+  value = aws_route_table.database.id  
 }
