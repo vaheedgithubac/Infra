14524b2b337f9687efe1c5ea3bc9a0a1a361a0f2
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-17 11:56:44 +0530

    Update main.tf

--- modules/aws_key_pair/main.tf
+++ modules/aws_key_pair/main.tf
@@ -12,7 +12,7 @@ resource "aws_key_pair" "generated" {
       echo "Current working Directory......{path.module}"
       echo "${tls_private_key.tf_generated_private_key.private_key_pem}" > ${path.module}/tf_aws_key_pair.pem
       chmod 400 tf_aws_key_pair.pem
-      echo "Private key created at ${path.module}/tf_aws_key_pair.pem"
+      echo "Private key created at: ${path.module}/tf_aws_key_pair.pem"
     EOF
   }
 }
