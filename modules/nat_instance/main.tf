8af647ed14052651cd175aaff1331b464551ee06
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-17 13:40:48 +0530

    Update main.tf

--- modules/nat_instance/main.tf
+++ modules/nat_instance/main.tf
@@ -11,6 +11,10 @@
   # depends_on = [aws_subnet.private]
 # }
 
+module "vpc" {
+  source = "git::https://github.com/vaheedgithubac/Infra//modules/vpc"
+}
+
 # Route table entry to forward traffic to NAT instance (This is the main step)
 resource "aws_route" "private_outbound_nat_route" {
   route_table_id         = module.vpc.private_route_table_id
