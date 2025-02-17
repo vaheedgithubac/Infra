c18e12493b7435368927a1c8616fced6765c8001
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-17 17:49:11 +0530

    Update ssm-parameters.tf

--- modules/ec2/ssm-parameters.tf
+++ modules/ec2/ssm-parameters.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "nat_primary_network_interface_id" {
   count = var.is_nat_instance ? 1 : 0
-  name  = "/${var.project_name}/${var.environment}/nat_primary_network_interface_id"
+  name  = "/${var.project_name}/${var.env}/nat_primary_network_interface_id"
   type  = "String"
   value = aws_instance.ec2_instance.primary_network_interface_id
 }
