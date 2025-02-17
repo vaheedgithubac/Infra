13a83308642e6c968e86166f5ff4a020b0da2830
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-17 17:42:39 +0530

    Update main.tf

--- modules/nat_instance/main.tf
+++ modules/nat_instance/main.tf
@@ -14,9 +14,9 @@
 
 # Route table entry to forward traffic to NAT instance (This is the main step)
 resource "aws_route" "private_outbound_nat_route" {
-  route_table_id         =  data.aws_ssm_parameter.private_route_table_id     # module.vpc.private_route_table_id
+  route_table_id         =  data.aws_ssm_parameter.private_route_table_id.value     # module.vpc.private_route_table_id
   destination_cidr_block = "0.0.0.0/0"
-  network_interface_id   =  data.aws_ssm_parameter.nat_primary_network_interface_id"  # aws_instance.nat_instance.primary_network_interface_id
+  network_interface_id   =  data.aws_ssm_parameter.nat_primary_network_interface_id.value # aws_instance.nat_instance.primary_network_interface_id
 }
 
 # resource "aws_route_table_association" "private" {
