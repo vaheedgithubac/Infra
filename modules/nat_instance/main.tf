3953ddf546c75a38ac01777cd4fc4b046f160ecb
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-18 11:48:37 +0530

    Update main.tf

--- modules/nat_instance/main.tf
+++ modules/nat_instance/main.tf
@@ -1,17 +1,3 @@
-# create private route table 
-# resource "aws_route_table" "private" {
-#  vpc_id = var.vpc_id
-
-# tags = merge(
-#    var.common_tags,
-#    {
-#      Name = "${local.resource_name}-private-RT"
-#    }
-#  )
-  # depends_on = [aws_subnet.private]
-# }
-
-
 # Route table entry to forward traffic from private instances to NAT instance (This is the main step)
 resource "aws_route" "private_outbound_nat_route" {
   route_table_id         =  data.aws_ssm_parameter.private_route_table_id.value     # module.vpc.private_route_table_id
@@ -25,13 +11,3 @@ resource "aws_route" "database_outbound_nat_route" {
   destination_cidr_block = "0.0.0.0/0"
   network_interface_id   =  data.aws_ssm_parameter.nat_primary_network_interface_id.value # aws_instance.nat_instance.primary_network_interface_id
 }
-
-# resource "aws_route_table_association" "private" {
-#  count = length(var.private_subnet_ids) > 0 ? length(var.private_subnet_ids) : 0
-
- # subnet_id      = var.private_subnet_ids[count.index]    #aws_subnet.private[count.index].id
-#  route_table_id = aws_route_table.private.id
-# }
-
-
-
