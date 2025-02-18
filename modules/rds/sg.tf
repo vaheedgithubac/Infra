38c96b70df874c56e10bf9dad172e71d61f7735f
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-18 18:44:39 +0530

    Create sg.tf

---
+++ modules/rds/sg.tf
@@ -0,0 +1,22 @@
+# Create a security group for MYSQL
+module "mysql_sg" {
+  source = "git::https://github.com/vaheedgithubac/Infra.git//modules/security_group"
+
+  vpc_id = var.vpc_id
+  sg_name = var.sg_name
+  description = var.sg_description
+
+  project_name = var.project_name
+  env = var.env
+  common_tags = var.common_tags
+}
+
+# MYSQL Security group rule to allow SSH from remote ip
+resource "aws_security_group_rule" "remote_admin" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = var.private_subnet_cidr     
+  security_group_id = module.mysql_sg.sg_id     # aws_security_group.sg_nat_instance.id
+}
