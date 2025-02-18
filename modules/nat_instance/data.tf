a42484fb61b2e8ce70e90869871ff9d3555270c0
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-18 12:40:53 +0530

    Update data.tf

--- modules/nat_instance/data.tf
+++ modules/nat_instance/data.tf
@@ -8,5 +8,5 @@ data "aws_ssm_parameter" "database_route_table_id" {
 
 data "aws_ssm_parameter" "nat_primary_network_interface_id" {
   name = "/${var.project_name}/${var.env}/nat_primary_network_interface_id"
-  depends_on = [module.ec2]
+  depends_on = [ module.ec2 ]
 }
