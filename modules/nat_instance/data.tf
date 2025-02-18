a96a970c55238e37dfa657b8e8df933c0a0aab1f
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-18 11:29:24 +0530

    Update data.tf

--- modules/nat_instance/data.tf
+++ modules/nat_instance/data.tf
@@ -2,6 +2,10 @@ data "aws_ssm_parameter" "private_route_table_id" {
   name = "/${var.project_name}/${var.env}/private_route_table_id"
 }
 
+data "aws_ssm_parameter" "database_route_table_id" {
+  name = "/${var.project_name}/${var.env}/database_route_table_id"
+}
+
 data "aws_ssm_parameter" "nat_primary_network_interface_id" {
   name = "/${var.project_name}/${var.env}/nat_primary_network_interface_id"
   depends_on = [module.ec2]
