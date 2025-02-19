41b6a321c89bf2d3f0bec9297d586a422bc04574
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-19 20:32:37 +0530

    Create locals.tf

---
+++ modules/asg_with_launch_template/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  launch_template_name     = "${var.asg_type}-${var.project_name}-${var.env}-launch-template"
+  auto_scaling_group_name  = "${var.asg_type}-${var.project_name}-${var.env}-autoscaling-group"
+  auto_scaling_policy_name = "${var.asg_type}-${var.project_name}-${var.env}-autoscaling-policy"
+}
