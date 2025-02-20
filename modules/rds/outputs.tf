4afa173d069e21d80cefc9a4fb02e6b1ff244496
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-20 17:24:26 +0530

    Create outputs.tf

---
+++ modules/rds/outputs.tf
@@ -0,0 +1,11 @@
+output "rds_address" {
+  value = aws_db_instance.db_instance.address
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.db_instance.endpoint
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.db_instance.availability_zone
+}
