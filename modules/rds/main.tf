0f836edc3f8a95edecebfcd8a195d989e1ef143f
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-20 18:18:15 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -22,11 +22,12 @@ resource "aws_db_instance" "db_instance" {
   password                = var.password
   instance_class          = var.instance_class
   allocated_storage       = var.allocated_storage
-  db_subnet_group_name    = aws_db_subnet_group.database_subnet_group.name
+  db_subnet_group_name    = var.db_subnet_group_name             # aws_db_subnet_group.database_subnet_group.name
   vpc_security_group_ids  = var.vpc_security_group_ids
   availability_zone       = var.availability_zone
   db_name                 = var.db_name
   skip_final_snapshot     = var.skip_final_snapshot
+  deletion_protection     = var.deletion_protection
 
   tags = merge(
     var.common_tags,
