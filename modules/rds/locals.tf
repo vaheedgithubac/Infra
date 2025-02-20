0e317bff730dd8fea558afb7c5ee9aedcb4ba94d
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-20 18:04:42 +0530

    Update locals.tf

--- modules/rds/locals.tf
+++ modules/rds/locals.tf
@@ -1,5 +1,4 @@
 locals {
   resource_final_name = "${var.project_name}-${var.env}-${var.sg_name}"    # expense-dev-<sg_name>
-  # private_subnet_ids  = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
   identifier = "${var.project_name}-${var.env}-db-instance"  # Mysql Instance Name
 }
