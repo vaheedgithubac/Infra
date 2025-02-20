bf5d37916b74a568a4517e27264ed21179f21780
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-20 17:05:47 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -17,13 +17,13 @@ resource "aws_db_instance" "db_instance" {
   engine                  = var.engine
   engine_version          = var.engine_version
   multi_az                = var.multi_az
-  identifier              = local.identifier
+  identifier              = var.identifier
   username                = var.username
   password                = var.password
   instance_class          = var.instance_class
   allocated_storage       = var.allocated_storage
   db_subnet_group_name    = aws_db_subnet_group.database_subnet_group.name
-  vpc_security_group_ids  = [module.mysql_sg.sg_id]   #var.vpc_security_group_ids
+  vpc_security_group_ids  = var.vpc_security_group_ids
   availability_zone       = var.availability_zone
   db_name                 = var.db_name
   skip_final_snapshot     = var.skip_final_snapshot
