245a5e392f3bb1ce4e187f8a40d61e93ef27260c
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-21 09:15:56 +0530

    Update nat_instance.tf

--- modules/nat_instance/nat_instance.tf
+++ modules/nat_instance/nat_instance.tf
@@ -4,12 +4,12 @@ module "ec2" {
   ami_id                      = var.ami_id
   public_key_name             = var.public_key_name
   sg_id                       = [module.nat_sg.sg_id]
-  subnet_id                   = var.public_subnet_ID_to_launch_nat_instance  #"subnet-088e8443a70102e2a" #1a
+  subnet_id                   = var.public_subnet_ID_to_launch_nat_instance  # "subnet-088e8443a70102e2a" #1a
+  associate_public_ip_address = true
+  is_nat_instance             = var.is_nat_instance  # creates NAT instance if true
+  # user_data                   = file("${path.module}/nat_user_data.sh")
+
   project_name                = var.project_name
   env                         = var.env
-  associate_public_ip_address = true
-  # user_data                   = file("${path.module}/nat_user_data.sh")
-  is_nat_instance             = var.is_nat_instance  # creates NAT instance if true
-
   common_tags                 = var.common_tags
 }
