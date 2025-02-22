b0dfc55397d52ff3d5134508937e055e94d8bead
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-22 19:27:17 +0530

    Update main.tf

--- modules/asg_with_launch_template/main.tf
+++ modules/asg_with_launch_template/main.tf
@@ -12,7 +12,7 @@ resource "aws_launch_template" "launch_template" {
 
   user_data = filebase64(var.user_data)         #filebase64("${path.module}/${var.user_data}")   
 
-  ebs { volume_size = 20 }
+  ebs { volume_size = 8 }
   iam_instance_profile { name = var.iam_instance_profile }
   network_interfaces { associate_public_ip_address = var.associate_public_ip_address }
 
