eb9aa936667d63a29efec2ae6511828e0414cd7d
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-22 19:38:12 +0530

    Create launch_template.tf

---
+++ modules/asg_with_launch_template/launch_template.tf
@@ -0,0 +1,32 @@
+# Create Launch Template
+resource "aws_launch_template" "launch_template" {
+
+  name                                 = local.launch_template_name
+  image_id                             = var.image_id   #aws_ami_from_instance.backend.id
+  instance_type                        = var.instance_type
+  key_name                             = var.key_name    # The key name to use for the instance
+  instance_initiated_shutdown_behavior = "terminate"
+
+  update_default_version = true
+  vpc_security_group_ids = [var.sg_id]
+
+  user_data = filebase64(var.user_data)         # filebase64("${path.module}/${var.user_data}")   
+
+  ebs { volume_size = 8 }
+  iam_instance_profile { name = var.iam_instance_profile }
+  network_interfaces { associate_public_ip_address = var.associate_public_ip_address }
+
+#   monitoring { enabled = true }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+      var.lt_tags,
+      var.common_tags,
+      {
+          Name = local.launch_template_name
+      }
+    )
+  }
+}
