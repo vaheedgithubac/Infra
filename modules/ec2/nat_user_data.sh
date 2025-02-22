31e1756ed1432454e30abb1109f46a110bb3e629
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-22 10:51:30 +0530

    Update nat_user_data.sh

--- modules/ec2/nat_user_data.sh
+++ modules/ec2/nat_user_data.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+echo " start of the nat user_data.sh script"
 sudo yum install iptables-services -y
 sudo systemctl enable iptables
 sudo systemctl start iptables
@@ -6,3 +7,4 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo /sbin/iptables -t nat -A POSTROUTING -o enX0 -j MASQUERADE
 # sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo /sbin/iptables -F FORWARD
+echo " reached end of the nat user_data.sh script"
