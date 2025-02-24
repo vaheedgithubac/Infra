af3d93f51908f2513bdbb9ee8c641ba63a3254fd
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-24 20:47:03 +0530

    Create variables.tf

---
+++ modules/launch_template/variables.tf
@@ -0,0 +1,20 @@
+#################### Common - Variables  ####################################################################
+### Tags
+variable "project_name" {}
+variable "env" {}
+variable "common_tags" { type = map(any) }
+#################### Launch Template - Variables  ###########################################################
+variable "image_id" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "vpc_security_group_ids" { type = list }
+variable "user_data" { default = null }
+variable "iam_instance_profile" { default = null }
+variable "associate_public_ip_address" { default = false }
+variable "lt_type" {}
+
+variable "lt_tags" {
+  type    = map(any)
+  default = {}
+}
+#############################################################################################################
