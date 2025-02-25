26b872245f76e8cae15a6935fa7ec9e76882fc5f
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 17:22:42 +0530

    Delete modules/autoscaling_group directory

--- modules/autoscaling_group/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  auto_scaling_group_name  = "${var.asg_type}-${var.project_name}-${var.env}-autoscaling-group"
-  auto_scaling_policy_name = "${var.asg_type}-${var.project_name}-${var.env}-autoscaling-policy"
-}
