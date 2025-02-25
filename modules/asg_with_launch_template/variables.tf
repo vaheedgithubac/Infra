ae329fc9fcf7861a3d9c88edd68b11617ecfe4a4
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 20:10:42 +0530

    Update variables.tf

--- modules/asg_with_launch_template/variables.tf
+++ modules/asg_with_launch_template/variables.tf
@@ -20,6 +20,6 @@ variable "lt_tags" {
 #################### ASG - Variables  ######################################################################
 variable "asg_type" {}                              # frontend / backend
 variable "target_group_arns" { type = list }        # For attaching ec2 instances to ASG
-variable "vpc_zone_identifier" { type = list(any) } # private / public  subnet_ids to launch ASG
+variable "vpc_zone_identifier" { type = list } # private / public  subnet_ids to launch ASG
 ############################################################################################################
 
