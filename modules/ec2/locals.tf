63af0dbd37ca32827e3718b265c2bf859515f309
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 15:39:01 +0530

    Update locals.tf

--- modules/ec2/locals.tf
+++ modules/ec2/locals.tf
@@ -1,4 +1,4 @@
 locals {
-    resource_final_name = var.is_nat_instance ? "NAT-INSTANCE-${var.project_name}-${var.env}" : "${var.what_type_instance}-${var.project_name}-${var.env}-ec2"                                   # expense-dev-ec2
+    resource_final_name = var.is_nat_instance ? "NAT-INSTANCE-${var.project_name}-${var.env}" : ( var.what_type_instance != null ? "${var.what_type_instance}-${var.project_name}-${var.env}-ec2" : "${var.project_name}-${var.env}-ec2")           # expense-dev-ec2
     root_volume_final_name = "${var.project_name}-${var.env}-ec2-root-volume"                   # expense-dev-ec2-root-volume
 }
