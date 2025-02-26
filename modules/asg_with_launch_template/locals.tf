fafdb35c3b2ccbc566b7e33cab5ac7c5cb511bf8
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 15:51:22 +0530

    Update locals.tf

--- modules/asg_with_launch_template/locals.tf
+++ modules/asg_with_launch_template/locals.tf
@@ -2,4 +2,6 @@ locals {
   launch_template_name     = "${var.lt_type}-${var.project_name}-${var.env}-launch-template"
   auto_scaling_group_name  = "${var.asg_type}-${var.project_name}-${var.env}-autoscaling-group"
   auto_scaling_policy_name = "${var.asg_type}-${var.project_name}-${var.env}-autoscaling-policy"
+
+  resource_name            = "${var.asg_type}-${var.project_name}-${var.env}"
 }
