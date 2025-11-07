b1255b68708bbe2d7b10abb6cd4dde953a3d6e97
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 19:36:48 +0530

    Update data.tf

--- modules/vpc_peering/data.tf
+++ modules/vpc_peering/data.tf
@@ -2,30 +2,30 @@
 # DATA SOURCES: Fetch VPC Details (like cidr_block and many more)
 ###############################################################################
 
-#data "aws_vpc" "requester" {
-#  id = var.requester_vpc_id
-#}
+data "aws_vpc" "requester" {
+  id = var.requester_vpc_id
+}
 
-#data "aws_vpc" "accepter" {
-#  id = var.accepter_vpc_id
-#}
+data "aws_vpc" "accepter" {
+  id = var.accepter_vpc_id
+}
 
 ###############################################################################
 # DATA SOURCES: Fetch all route tables in a given VPC
 ###############################################################################
 
-#data "aws_route_tables" "requester" {
+data "aws_route_tables" "requester" {
   # vpc_id = var.requester_vpc_id
-#  filter {
-#    name   = "vpc-id"
-#    values = [var.requester_vpc_id]
-#  }
-#}
+  filter {
+    name   = "vpc-id"
+    values = [var.requester_vpc_id]
+  }
+}
 
-#data "aws_route_tables" "accepter" {
+data "aws_route_tables" "accepter" {
   # vpc_id = var.accepter_vpc_id
- # filter {
- #   name   = "vpc-id"
- #   values = [var.accepter_vpc_id]
- # }
-#}
+  filter {
+    name   = "vpc-id"
+    values = [var.accepter_vpc_id]
+  }
+}
