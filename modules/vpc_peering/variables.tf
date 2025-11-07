eaaccd9a4c676a685d49ed96a7ffda3a700f37cd
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 12:05:28 +0530

    Update variables.tf

--- modules/vpc_peering/variables.tf
+++ modules/vpc_peering/variables.tf
@@ -1,5 +1,9 @@
 variable "region" {}
 
 variable "requester_vpc_id" {}
-
 variable "accepter_vpc_id" {}
+
+variable "requester_vpc_cidr" {}
+variable "accepter_vpc_cidr" {}
+
+variable "route_table_ids" { type = list }
