ac5dc6c3c8da8cb348a984cc1ba6c4e0cbcc7757
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 08:52:46 +0530

    Update main.tf

--- modules/vpc_peering/main.tf
+++ modules/vpc_peering/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "peering" {
 
-  vpc_id      = var.requester_vpc_id
-  peer_vpc_id = var.accepter_vpc_id
+  vpc_id      = var.requester_vpc_id      # requester VPC
+  peer_vpc_id = var.accepter_vpc_id       # accepter VPC
   auto_accept = true
 
   tags = {
@@ -15,8 +15,8 @@ resource "aws_route" "requester" {
   count = length(data.aws_route_tables.requester.ids)
 
   route_table_id         = tolist(data.aws_route_tables.requester.ids)[count.index]
-  destination_cidr_block = var.peering_dest_cidr_accepter
   # since we set count parameter, it is treated as list, even single element you should write list syntax
+  destination_cidr_block = var.peering_dest_cidr_accepter
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
@@ -25,8 +25,8 @@ resource "aws_route" "accepter" {
   count = length(data.aws_route_tables.accepter.ids)
 
   route_table_id         = tolist(data.aws_route_tables.accepter.ids)[count.index]
-  destination_cidr_block = var.peering_dest_cidr_requester
   # since we set count parameter, it is treated as list, even single element you should write list syntax
+  destination_cidr_block = var.peering_dest_cidr_requester
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 
 }
