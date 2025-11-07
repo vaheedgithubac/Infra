46d2c815adee468b208bfa0554a677105b264fb1
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 09:39:55 +0530

    Update data.tf

--- modules/vpc_peering/data.tf
+++ modules/vpc_peering/data.tf
@@ -18,6 +18,6 @@ data "aws_route_tables" "requester" {
   vpc_id = var.requester_vpc_id
 }
 
- data "aws_route_tables" "accepter" {
+data "aws_route_tables" "accepter" {
   vpc_id = var.accepter_vpc_id
- }
+}
