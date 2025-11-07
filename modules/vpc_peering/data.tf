548865afb31822496d1c83dbbb5e758ee2894a9e
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 09:14:52 +0530

    Update data.tf

--- modules/vpc_peering/data.tf
+++ modules/vpc_peering/data.tf
@@ -1,22 +1,26 @@
 # Query vpc_id from AWS_SSM_PARAMETER store
 # in /<project_name>/<env>/<vpc_id>
 
-# data "aws_ssm_parameter" "requester_vpc_id" {
-#  name = var.requester_vpc_id
-#  #depends_on = [ module.vpc.aws_ssm_parameter.vpc_id ]
-# }
+###############################################################################
+# DATA SOURCES: Fetch VPC Details
+###############################################################################
 
-# data "aws_ssm_parameter" "accepter_vpc_id" {
-#  name = var.accepter_vpc_id
-  #depends_on = [ module.vpc.aws_ssm_parameter.vpc_id ]
-# }
+data "aws_vpc" "requester" {
+  id = var.requester_vpc_id
+}
+
+data "aws_vpc" "accepter" {
+  id = var.accepter_vpc_id
+}
+
+###############################################################################
+# DATA SOURCES: Fetch all route tables in a given VPC
+###############################################################################
 
 data "aws_route_tables" "requester" {
   vpc_id = var.requester_vpc_id
-  #depends_on = [ module.vpc.vpc_id ]
- }
+}
 
  data "aws_route_tables" "accepter" {
   vpc_id = var.accepter_vpc_id
-  #depends_on = [ module.vpc.vpc_id ]
  }
