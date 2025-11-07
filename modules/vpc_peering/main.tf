cb15a0ee24dbc6f4d51718acba3ef5950aeea7c1
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 11:18:06 +0530

    Update main.tf

--- modules/vpc_peering/main.tf
+++ modules/vpc_peering/main.tf
@@ -12,18 +12,20 @@ resource "aws_vpc_peering_connection" "peering" {
 
 # updating destination vpc_cidr route in all the route tables of "requester vpc"
 resource "aws_route" "requester" {
-  count = length(tolist(local.requester_vpc_route_table_ids))
+  #count = length(tolist(local.requester_vpc_route_table_ids))
+  for_each = { for idx, id in local.requester_vpc_route_table_ids : idx => id }
 
-  route_table_id         = local.requester_vpc_route_table_ids[count.index]                                  
+  route_table_id         = each.value                            # local.requester_vpc_route_table_ids[count.index]                                  
   destination_cidr_block = data.aws_vpc.accepter.cidr_block     
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
 # updating destination vpc_cidr route in all the route tables of "accepter vpc"
 resource "aws_route" "accepter" {
-  count = length(tolist(local.accepter_vpc_route_table_ids))
+  #count = length(tolist(local.accepter_vpc_route_table_ids))
+  for_each = { for idx, id in local.accepter_vpc_route_table_ids : idx => id }
 
-  route_table_id         = local.accepter_vpc_route_table_ids[count.index]                                    
+  route_table_id         = each.value                            # local.accepter_vpc_route_table_ids[count.index]                                    
   destination_cidr_block = data.aws_vpc.requester.cidr_block      
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 
