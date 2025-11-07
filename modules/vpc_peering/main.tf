8fbcda1cd4d32e67a6b6aa19417aa5152e34ffe4
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 09:27:57 +0530

    Update main.tf

--- modules/vpc_peering/main.tf
+++ modules/vpc_peering/main.tf
@@ -12,21 +12,19 @@ resource "aws_vpc_peering_connection" "peering" {
 
 # updating destination vpc_cidr route in all the route tables of "requester vpc"
 resource "aws_route" "requester" {
-  count = length(data.aws_route_tables.requester.ids)
+  for_each = toset(data.aws_route_tables.requester.ids)
 
-  route_table_id         = tolist(data.aws_route_tables.requester.ids)[count.index]
-  # since we set count parameter, it is treated as list, even single element you should write list syntax
-  destination_cidr_block = var.peering_dest_cidr_accepter
+  route_table_id         = each.value                                  
+  destination_cidr_block = data.aws_vpc.accepter.cidr_block     
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
 # updating destination vpc_cidr route in all the route tables of "accepter vpc"
 resource "aws_route" "accepter" {
-  count = length(data.aws_route_tables.accepter.ids)
+  for_each = toset(data.aws_route_tables.accepter.ids)
 
-  route_table_id         = tolist(data.aws_route_tables.accepter.ids)[count.index]
-  # since we set count parameter, it is treated as list, even single element you should write list syntax
-  destination_cidr_block = var.peering_dest_cidr_requester
+  route_table_id         = each.value                                    
+  destination_cidr_block = data.aws_vpc.requester.cidr_block      
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 
 }
